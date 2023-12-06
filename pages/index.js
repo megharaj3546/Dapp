b80fb9bfe9c2f33ca1df71b5697f4d4090f560b7
megharaj3546 <152140532+megharaj3546@users.noreply.github.com>
2023-12-06 15:56:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
@@ -7,86 +7,159 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [showReceipt, setShowReceipt] = useState(false);
+  const [receiptData, setReceiptData] = useState({
+    accountHolderName: "Megharaj",
+    address: "123 Church st ,Banglore",
+    amountType: "",
+    amount: 0,
+    balance: 0,
+    time: "",
+    date: "",
+    location: "ATM Location",
+  });
+
+  const [transactionHistory, setTransactionHistory] = useState([]);
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async() => {
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
+      const account = await ethWallet.request({ method: "eth_accounts" });
       handleAccount(account);
     }
-  }
+  };
 
   const handleAccount = (account) => {
     if (account) {
-      console.log ("Account connected: ", account);
+      console.log("Account connected: ", account);
       setAccount(account);
-    }
-    else {
+    } else {
       console.log("No account found");
     }
-  }
+  };
 
-  const connectAccount = async() => {
+  const connectAccount = async () => {
     if (!ethWallet) {
-      alert('MetaMask wallet is required to connect');
+      alert("MetaMask wallet is required to connect");
       return;
     }
-  
-    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
+
+    const accounts = await ethWallet.request({
+      method: "eth_requestAccounts",
+    });
     handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
+
     getATMContract();
   };
 
   const getATMContract = () => {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
-    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
-    setATM(atmContract);
-  }
+    const atmContract = new ethers.Contract(
+      contractAddress,
+      atmABI,
+      signer
+    );
 
-  const getBalance = async() => {
+    setATM(atmContract);
+  };
+
+  const getBalance = async () => {
     if (atm) {
       setBalance((await atm.getBalance()).toNumber());
     }
-  }
+  };
 
-  const deposit = async() => {
+  const deposit = async () => {
     if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
+      let tx = await atm.deposit(100); // Prompting user to deposit 100 ETH
+      await tx.wait();
       getBalance();
+      showTransactionReceipt("Deposit", 100);
     }
-  }
+  };
 
-  const withdraw = async() => {
+  const withdraw = async () => {
     if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
+      let tx = await atm.withdraw(50); // Prompting user to withdraw 50 ETH
+      await tx.wait();
       getBalance();
+      showTransactionReceipt("Withdrawal", 50);
     }
-  }
+  };
+
+  const showTransactionReceipt = (type, amount) => {
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString();
+    const formattedTime = now.toLocaleTimeString();
+
+    const newReceipt = {
+      accountHolderName: "Megharaj", 
+      address: "123 Church St, Banglore", 
+      amountType: type,
+      amount: amount,
+      balance: balance,
+      time: formattedTime,
+      date: formattedDate,
+      location: "ATM Location",
+    };
+
+    setReceiptData(newReceipt);
+    setShowReceipt(true);
+
+    // Store the transaction details in the history
+    setTransactionHistory((prevHistory) => [newReceipt, ...prevHistory]);
+  };
+
+  const hideReceipt = () => {
+    setShowReceipt(false);
+  };
+
+  const showRecentReceipt = () => {
+    if (transactionHistory.length > 0) {
+      setReceiptData(transactionHistory[0]);
+      setShowReceipt(true);
+    }
+  };
+
+  const renderReceipt = () => {
+    return (
+      <div className="receipt">
+        <h2>Receipt</h2>
+        <p>Account Holder: {receiptData.accountHolderName}</p>
+        <p>Address: {receiptData.address}</p>
+        <p>Amount Type: {receiptData.amountType}</p>
+        <p>Amount: {receiptData.amount} ETH</p>
+        <p>Balance: {receiptData.balance} ETH</p>
+        <p>Date: {receiptData.date}</p>
+        <p>Time: {receiptData.time}</p>
+        <p>Location: {receiptData.location}</p>
+      </div>
+    );
+  };
 
   const initUser = () => {
-    // Check to see if user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
+      return (
+        <p>Please install Metamask in order to use this ATM.</p>
+      );
     }
 
-    // Check to see if user is connected. If not, connect to their account
     if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      return (
+        <button onClick={connectAccount}>
+          Please connect your Metamask wallet
+        </button>
+      );
     }
 
-    if (balance == undefined) {
+    if (balance === undefined) {
       getBalance();
     }
 
@@ -94,24 +167,68 @@ export default function HomePage() {
       <div>
         <p>Your Account: {account}</p>
         <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <button onClick={deposit} style={{ background: 'blue', color: 'white' }}>
+          Deposit 100 ETH
+        </button>
+        <button onClick={withdraw} style={{ background: 'brown', color: 'white' }}>
+          Withdraw 50 ETH
+        </button>
+        <button
+          onClick={showRecentReceipt}
+          style={{ background: 'green', color: 'white' }}
+        >
+          Show Recent Receipt
+        </button>
       </div>
-    )
-  }
+    );
+  };
 
-  useEffect(() => {getWallet();}, []);
+  useEffect(() => {
+    getWallet();
+  }, []);
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      <header>
+        <h1>Welcome to the MetaATM Experience!</h1>
+        <p>Empowering Your Digital Finances with Blockchain</p>
+      </header>
       {initUser()}
+      {showReceipt && (
+        <div className="modal">
+          <div className="modal-content">
+            {renderReceipt()}
+            <button onClick={hideReceipt}>Close Receipt</button>
+          </div>
+        </div>
+      )}
       <style jsx>{`
         .container {
-          text-align: center
+          text-align: center;
         }
-      `}
-      </style>
+
+        .modal {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, 0.5);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .modal-content {
+          background: white;
+          padding: 20px;
+          border-radius: 8px;
+        }
+
+        .receipt {
+          text-align: left;
+        }
+      `}</style>
     </main>
-  )
+  );
 }
